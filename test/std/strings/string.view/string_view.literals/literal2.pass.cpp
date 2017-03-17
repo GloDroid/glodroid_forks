8fe43059c4392a0c863b8551d2ac3eb2eb7a66ed
Tom Cherry <tomcherry@google.com>
2017-03-17 15:33:19 -0700

    Update XFAIL
    The latest clang in our tree now passes the below four tests:
    
        libc++ :: std/atomics/atomics.types.operations/atomics.types.operations.req/atomic_compare_exchange_strong.pass.cpp
        libc++ :: std/atomics/atomics.types.operations/atomics.types.operations.req/atomic_compare_exchange_strong_explicit.pass.cpp
        libc++ :: std/atomics/atomics.types.operations/atomics.types.operations.req/atomic_compare_exchange_weak.pass.cpp
        libc++ :: std/atomics/atomics.types.operations/atomics.types.operations.req/atomic_compare_exchange_weak_explicit.pass.cpp
    
    It however fails the below six:
    
        libc++ :: std/strings/string.view/string_view.literals/literal.pass.cpp
        libc++ :: std/strings/string.view/string_view.literals/literal1.pass.cpp
        libc++ :: std/strings/string.view/string_view.literals/literal2.pass.cpp
        libc++ :: std/strings/string.view/string_view.literals/literal3.pass.cpp
        libc++ :: std/strings/basic.string/string.cons/implicit_deduction_guides.pass.cpp
        libc++ :: std/strings/string.view/string.view.cons/implicit_deduction_guides.pass.cpp
    
    Bug: 33278386
    Bug: 36400049
    Bug: 36401676
    Test: ./run_tests.py
    Change-Id: I24394534ba77ecbc2cdc682ac5e681baefb11c16

--- test/std/strings/string.view/string_view.literals/literal2.pass.cpp
+++ test/std/strings/string.view/string_view.literals/literal2.pass.cpp
@@ -12,6 +12,10 @@
 // UNSUPPORTED: clang-3.3, clang-3.4, clang-3.5, clang-3.6, clang-3.7, clang-3.8, clang-3.9
 // UNSUPPORTED: apple-clang-6, apple-clang-7, apple-clang-8
 // Note: libc++ supports string_view before C++17, but literals were introduced in C++14
+//
+// This will fail before r290744
+// http://b/36400049
+// XFAIL: *
 
 #include <string_view>
 #include <cassert>
