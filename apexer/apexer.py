7c10f419576191afe782792d4529be85f965d008
Jiyong Park <jiyong@google.com>
2018-08-21 21:28:41 +0900

    Files in APEX are correctly labeled
    add '--file_contexts' option to label files in the image.img in an APEX.
    
    Bug: 112458021
    Test: m -j
    Test: runtests.sh
    Change-Id: I6c96f2815560528fe89199c7efb88d553c245a8f

--- apexer/apexer.py
+++ apexer/apexer.py
@@ -39,6 +39,8 @@ def ParseArgs(argv):
                       help='verbose execution')
   parser.add_argument('--manifest', default='manifest.json',
                       help='path to the APEX manifest file')
+  parser.add_argument('--file_contexts',
+                      help='selinux file contexts file')
   parser.add_argument('input_dir', metavar='INPUT_DIR',
                       help='the directory having files to be packaged')
   parser.add_argument('output', metavar='OUTPUT',
@@ -124,7 +126,7 @@ def CreateApex(args, work_dir):
     return False
   package_name = manifest['name']
 
-  # Step 1: create an empty ext4 image that is sufficiently big
+  # create an empty ext4 image that is sufficiently big
   # Sufficiently big = twice the size of the input directory
   # For the case when the input directory is really small, the minimum of the
   # size is set to 10MB that is sufficiently large for filesystem metadata
@@ -144,10 +146,19 @@ def CreateApex(args, work_dir):
   cmd.append(str(size_in_mb) + 'M')
   RunCommand(cmd, args.verbose)
 
-  # Step 2: Add files to the image file
+  # Compile the file context into the binary form
+  compiled_file_contexts = os.path.join(work_dir, 'file_contexts.bin')
+  cmd = ['sefcontext_compile']
+  cmd.extend(['-o', compiled_file_contexts])
+  cmd.append(args.file_contexts)
+  RunCommand(cmd, args.verbose)
+
+  # Add files to the image file
   cmd = ['e2fsdroid']
   cmd.append('-e') # input is not android_sparse_file
   cmd.extend(['-f', args.input_dir])
+  cmd.extend(['-T', '0']) # time is set to epoch
+  cmd.extend(['-S', compiled_file_contexts])
   cmd.append(img_file)
   RunCommand(cmd, args.verbose)
 
@@ -164,16 +175,18 @@ def CreateApex(args, work_dir):
   cmd = ['e2fsdroid']
   cmd.append('-e') # input is not android_sparse_file
   cmd.extend(['-f', manifests_dir])
+  cmd.extend(['-T', '0']) # time is set to epoch
+  cmd.extend(['-S', compiled_file_contexts])
   cmd.append(img_file)
   RunCommand(cmd, args.verbose)
 
-  # Step 3: Resize the image file to save space
+  # Resize the image file to save space
   cmd = ['resize2fs']
   cmd.append('-M') # shrink as small as possible
   cmd.append(img_file)
   RunCommand(cmd, args.verbose)
 
-  # Step 4: package the image file and APEX manifest as an APK.
+  # package the image file and APEX manifest as an APK.
   # The AndroidManifest file is automatically generated.
   android_manifest_file = os.path.join(work_dir, 'AndroidManifest.xml')
   if args.verbose:
@@ -209,7 +222,7 @@ def CreateApex(args, work_dir):
   cmd.append(zip_file) # input
   RunCommand(cmd, args.verbose)
 
-  # Step 5: Align the files at page boundary for efficient access
+  # Align the files at page boundary for efficient access
   cmd = ['zipalign']
   cmd.append('-f')
   cmd.append('4096') # 4k alignment
--- apexer/runtests.sh
+++ apexer/runtests.sh
@@ -13,13 +13,14 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
 
 if [ -z $ANDROID_BUILD_TOP ]; then
   echo "You need to source and lunch before you can use this script"
   exit 1
 fi
 
-echo "Running tests"
+echo "Running test"
 set -e # fail early
 
 source $ANDROID_BUILD_TOP/build/envsetup.sh
@@ -37,32 +38,62 @@ function finish {
 }
 
 trap finish EXIT
-
+#############################################
+# prepare the inputs
+#############################################
+# Create the input directory having 3 files with random bits
 head -c 1M </dev/urandom > ${input_dir}/file1
 head -c 1M </dev/urandom > ${input_dir}/file2
 mkdir ${input_dir}/sub
 head -c 1M </dev/urandom > ${input_dir}/sub/file3
 
+# Create the APEX manifest file
 manifest_file=$(mktemp)
 echo '{"name": "com.android.example.apex", "version": 1}' > ${manifest_file}
 
+# Create the file_contexts file
+file_contexts_file=$(mktemp)
+echo '
+(/.*)?           u:object_r:root_file:s0
+/sub(/.*)?       u:object_r:sub_file:s0
+/sub/file3       u:object_r:file3_file:s0
+' > ${file_contexts_file}
+
 output_file=${output_dir}/test.apex
 
-${ANDROID_HOST_OUT}/bin/apexer --verbose --manifest ${manifest_file} ${input_dir} ${output_file}
+#############################################
+# run the tool
+#############################################
+${ANDROID_HOST_OUT}/bin/apexer --verbose --manifest ${manifest_file} \
+  --file_contexts ${file_contexts_file} \
+  ${input_dir} ${output_file}
 
+#############################################
+# check the result
+#############################################
 offset=$(zipalign -v -c 4096 ${output_file} | grep image.img | tr -s ' ' | cut -d ' ' -f 2)
 
+# test if it is mountable
 mkdir ${output_dir}/mnt
 sudo losetup -o ${offset} /dev/loop10 ${output_file}
 sudo mount -o ro /dev/loop10 ${output_dir}/mnt
 unzip ${output_file} manifest.json -d ${output_dir}
 
+# check the contents
 sudo diff ${manifest_file} ${output_dir}/mnt/manifest.json
 diff ${manifest_file} ${output_dir}/manifest.json
 sudo diff ${input_dir}/file1 ${output_dir}/mnt/file1
 sudo diff ${input_dir}/file2 ${output_dir}/mnt/file2
 sudo diff ${input_dir}/sub/file3 ${output_dir}/mnt/sub/file3
 
+# check the selinux labels
+[ `ls -Z ${output_dir}/mnt/file1 | cut -d ' ' -f 1` = "u:object_r:root_file:s0" ]
+[ `ls -Z ${output_dir}/mnt/file2 | cut -d ' ' -f 1` = "u:object_r:root_file:s0" ]
+[ `ls -d -Z ${output_dir}/mnt/sub/ | cut -d ' ' -f 1` = "u:object_r:sub_file:s0" ]
+[ `ls -Z ${output_dir}/mnt/sub/file3 | cut -d ' ' -f 1` = "u:object_r:file3_file:s0" ]
+[ `ls -Z ${output_dir}/mnt/manifest.json | cut -d ' ' -f 1` = "u:object_r:root_file:s0" ]
+
+# check the android manifest
 aapt dump xmltree ${output_file} AndroidManifest.xml | grep com.android.example.apex
 
 echo Passed
