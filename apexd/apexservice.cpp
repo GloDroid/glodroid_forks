8e88b68ca16ab431f1d9dd7356a5126b40395b1a
Dario Freni <dariofreni@google.com>
2018-10-25 13:06:21 +0100

    Add getActivePackages binder method.
    For now, this goes through the mounted directories and parses the
    package name and version. This will change once we have an internal
    database.
    
    Test: on device
    Bug: 117589375
    Change-Id: If0abb44718ece39580e812560a95dfe90f2a7d45

--- apexd/apexservice.cpp
+++ apexd/apexservice.cpp
@@ -18,12 +18,15 @@
 
 #include <fstream>
 
+#include <dirent.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 #include <android-base/file.h>
 #include <android-base/logging.h>
 #include <android-base/properties.h>
 #include <android-base/stringprintf.h>
+#include <android-base/strings.h>
 #include <binder/IResultReceiver.h>
 
 #include "apexd.h"
@@ -46,10 +49,12 @@ BinderStatus CheckDebuggable() {
   return BinderStatus::ok();
 }
 
-}
+}  // namespace
 
-BinderStatus ApexService::installPackage(const std::string& packageTmpPath, bool* aidl_return) {
-  LOG(DEBUG) << "installPackage() received by ApexService, path " << packageTmpPath;
+BinderStatus ApexService::installPackage(const std::string& packageTmpPath,
+                                         bool* aidl_return) {
+  LOG(DEBUG) << "installPackage() received by ApexService, path "
+             << packageTmpPath;
 
   *aidl_return = false;
   Status res = ::android::apex::installPackage(packageTmpPath);
@@ -60,7 +65,8 @@ BinderStatus ApexService::installPackage(const std::string& packageTmpPath, bool
   }
 
   // TODO: Get correct binder error status.
-  LOG(ERROR) << "Failed installing " << packageTmpPath << ": " << res.ErrorMessage();
+  LOG(ERROR) << "Failed installing " << packageTmpPath << ": "
+             << res.ErrorMessage();
   return BinderStatus::fromExceptionCode(BinderStatus::EX_ILLEGAL_ARGUMENT,
                                          String8(res.ErrorMessage().c_str()));
 }
@@ -85,10 +91,44 @@ BinderStatus ApexService::mountPackage(const std::string& packagePath) {
                                          String8(res.ErrorMessage().c_str()));
 }
 
-status_t ApexService::onTransact(uint32_t _aidl_code,
-                                 const Parcel& _aidl_data,
-                                 Parcel* _aidl_reply,
-                                 uint32_t _aidl_flags) {
+BinderStatus ApexService::getActivePackages(
+    std::vector<PackageInfo>* aidl_return) {
+  LOG(DEBUG) << "Scanning " << kApexRoot
+             << " looking for packages already installed.";
+  // This code would be much shorter if C++17's std::filesystem were available,
+  // which is not at the time of writing this.
+  auto d = std::unique_ptr<DIR, int (*)(DIR*)>(opendir(kApexRoot), closedir);
+  if (!d) {
+    PLOG(ERROR) << "Can't open " << kApexRoot << " for reading.";
+    return BinderStatus::fromExceptionCode(
+        BinderStatus::EX_ILLEGAL_STATE,
+        "Internal error, apex root directory is not readable or doesn't "
+        "exist.");
+  }
+
+  struct dirent* dp;
+  while ((dp = readdir(d.get())) != NULL) {
+    if (dp->d_type != DT_DIR || (strcmp(dp->d_name, ".") == 0) ||
+        (strcmp(dp->d_name, "..") == 0)) {
+      continue;
+    }
+    PackageInfo pkg;
+    std::vector<std::string> splits = android::base::Split(dp->d_name, "@");
+    if (splits.size() != 2) {
+      LOG(ERROR) << "Unable to extract package info from directory name "
+                 << dp->d_name << "... skipping.";
+      continue;
+    }
+
+    pkg.package_name = splits[0];
+    pkg.version_code = atol(splits[1].c_str());
+    aidl_return->push_back(pkg);
+  }
+  return BinderStatus::ok();
+}
+
+status_t ApexService::onTransact(uint32_t _aidl_code, const Parcel& _aidl_data,
+                                 Parcel* _aidl_reply, uint32_t _aidl_flags) {
   switch (_aidl_code) {
     case IBinder::SHELL_COMMAND_TRANSACTION: {
       int in = _aidl_data.readFileDescriptor();
@@ -113,29 +153,55 @@ status_t ApexService::onTransact(uint32_t _aidl_code,
       return OK;
     }
   }
-  return BnApexService::onTransact(_aidl_code, _aidl_data, _aidl_reply, _aidl_flags);
+  return BnApexService::onTransact(_aidl_code, _aidl_data, _aidl_reply,
+                                   _aidl_flags);
 }
 
-status_t ApexService::shellCommand(int in, int out, int err, const Vector<String16>& args) {
+status_t ApexService::shellCommand(int in, int out, int err,
+                                   const Vector<String16>& args) {
   if (in == BAD_TYPE || out == BAD_TYPE || err == BAD_TYPE) {
     return BAD_VALUE;
   }
   auto print_help = [](int fd) {
     auto stream = std::fstream(base::StringPrintf("/proc/self/fd/%d", fd));
-    stream << "ApexService:" << std::endl
-           << "  help - display this help" << std::endl
-           << "  installPackage [packagePath] - install package from the given path" << std::endl
-           << "  mountPackage [packagePath]   - mount package from the given path" << std::endl;
+    stream
+        << "ApexService:" << std::endl
+        << "  help - display this help" << std::endl
+        << "  installPackage [packagePath] - install package from the given "
+           "path"
+        << std::endl
+        << "  getActivePackages - return the list of active packages"
+        << std::endl
+        << "  mountPackage [packagePath]   - mount package from the given path"
+        << std::endl;
   };
 
   if (args.size() == 2 && args[0] == String16("installPackage")) {
     bool ret_value;
-    ::android::binder::Status status = installPackage(String8(args[1]).string(), &ret_value);
+    ::android::binder::Status status =
+        installPackage(String8(args[1]).string(), &ret_value);
     if (status.isOk()) {
       return OK;
     }
     auto err_str = std::fstream(base::StringPrintf("/proc/self/fd/%d", err));
-    err_str << "Failed to install package: " << status.toString8().string() << std::endl;
+    err_str << "Failed to install package: " << status.toString8().string()
+            << std::endl;
+    return BAD_VALUE;
+  }
+  if (args.size() == 1 && args[0] == String16("getActivePackages")) {
+    std::vector<PackageInfo> list;
+    android::binder::Status status = getActivePackages(&list);
+    if (status.isOk()) {
+      auto out_str = std::fstream(base::StringPrintf("/proc/self/fd/%d", out));
+      for (auto item : list) {
+        out_str << "Package: " << item.package_name
+                << " Version: " << item.version_code << std::endl;
+      }
+      return OK;
+    }
+    auto err_str = std::fstream(base::StringPrintf("/proc/self/fd/%d", err));
+    err_str << "Failed to retrieve packages: " << status.toString8().string()
+            << std::endl;
     return BAD_VALUE;
   }
   if (args.size() == 2 && args[0] == String16("mountPackage")) {
@@ -144,7 +210,8 @@ status_t ApexService::shellCommand(int in, int out, int err, const Vector<String
       return OK;
     }
     auto err_str = std::fstream(base::StringPrintf("/proc/self/fd/%d", err));
-    err_str << "Failed to mount package: " << status.toString8().string() << std::endl;
+    err_str << "Failed to mount package: " << status.toString8().string()
+            << std::endl;
     return BAD_VALUE;
   }
   if (args.size() == 1 && args[0] == String16("help")) {
