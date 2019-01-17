6112ed3b39637d7832f03640781e28ebb8e87793
Chen Zhu <chenzhu@google.com>
2019-01-17 13:04:08 -0800

    Apex e2e test: automatically retrieve package name and version code from apex file.
    Test: atest apex_e2e_tests
          atest timezone_data_e2e_tests (download test1_com.android.tzdata.apex and copy to $ANDROID_HOST_OUT_TESTCASES
    first)
    
    Bug 120920920
    Bug 120922171
    
    Change-Id: I78fe90d2b8501f8a7679cb906a0e5f6993ef59fc

--- tests/src/com/android/tests/apex/ApexE2EBaseHostTest.java
+++ tests/src/com/android/tests/apex/ApexE2EBaseHostTest.java
@@ -23,7 +23,11 @@ import com.android.tradefed.device.DeviceNotAvailableException;
 import com.android.tradefed.device.ITestDevice.ApexInfo;
 import com.android.tradefed.log.LogUtil.CLog;
 import com.android.tradefed.testtype.junit4.BaseHostJUnit4Test;
+import com.android.tradefed.util.CommandResult;
+import com.android.tradefed.util.CommandStatus;
 import com.android.tradefed.util.FileUtil;
+import com.android.tradefed.util.IRunUtil;
+import com.android.tradefed.util.RunUtil;
 import com.android.tradefed.util.SystemUtil.EnvVariable;
 
 import org.junit.After;
@@ -33,6 +37,8 @@ import org.junit.Before;
 import java.io.File;
 import java.io.IOException;
 import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Base test to check if Apex can be staged, activated and uninstalled successfully.
@@ -40,18 +46,9 @@ import java.util.Set;
 public abstract class ApexE2EBaseHostTest extends BaseHostJUnit4Test {
     private static final String APEX_DATA_DIR = "/data/apex";
     private static final String STAGING_DATA_DIR = "/data/staging";
-    private static final String OPTION_APEX_PACKAGE_NAME = "apex_package_name";
     private static final String OPTION_APEX_FILE_NAME = "apex_file_name";
-    private static final String OPTION_APEX_VERSION = "apex_version_code";
-
-    private boolean mAdbWasRoot;
-
-    @Option(name = OPTION_APEX_PACKAGE_NAME,
-            description = "The package name of the apex module. Specified in manifest.json.",
-            importance = Importance.IF_UNSET,
-            mandatory = true
-    )
-    private String mApexPackageName = null;
+    private static final String APEX_INFO_EXTRACT_REGEX =
+            ".*package:\\sname='(\\S+)\\'\\sversionCode='(\\d+)'\\s.*";
 
     @Option(name = OPTION_APEX_FILE_NAME,
             description = "The file name of the apex module.",
@@ -60,13 +57,7 @@ public abstract class ApexE2EBaseHostTest extends BaseHostJUnit4Test {
     )
     private String mApexFileName = null;
 
-    @Option(name = OPTION_APEX_VERSION,
-            description = "The version code of the apex module.",
-            importance = Importance.IF_UNSET,
-            mandatory = true
-    )
-    private long mApexVersion;
-
+    private IRunUtil mRunUtil = new RunUtil();
 
     @Before
     public synchronized void setUp() throws Exception {
@@ -88,22 +79,55 @@ public abstract class ApexE2EBaseHostTest extends BaseHostJUnit4Test {
                 String.format("failed to install test app %s. Reason: %s",
                     mApexFileName, installResult),
                 installResult);
+
         // TODO(b/122882120) wait for the "isReady" broadcast.
         try {
             Thread.sleep(10000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+
+        ApexInfo testApexInfo = getApexInfo(testAppFile);
+        Assert.assertNotNull(testApexInfo);
+
         getDevice().reboot();
 
-        // Check that the APEX is actually activated
-        ApexInfo testApexInfo = new ApexInfo(mApexPackageName, mApexVersion);
         Set<ApexInfo> activatedApexes = getDevice().getActiveApexes();
         Assert.assertTrue(
-                String.format("Failed to activate %s", mApexPackageName),
+                String.format("Failed to activate %s %s",
+                    testApexInfo.name, testApexInfo.versionCode),
                 activatedApexes.contains(testApexInfo));
 
         additionalCheck();
+    }
+
+    /*
+     * Retrieve package name and version code from test apex file.
+     */
+    private ApexInfo getApexInfo(File apex) {
+        String aaptOutput = runCmd(String.format(
+                "aapt dump badging %s", apex.getAbsolutePath()));
+        String[] lines = aaptOutput.split("\n");
+        Pattern p = Pattern.compile(APEX_INFO_EXTRACT_REGEX);
+        for (String l : lines) {
+            Matcher m = p.matcher(l);
+            if (m.matches()) {
+                ApexInfo apexInfo = new ApexInfo(m.group(1), Long.parseLong(m.group(2)));
+                return apexInfo;
+            }
+        }
+        return null;
+    }
+
+    private String runCmd(String cmd) {
+        CLog.d("About to run command: %s", cmd);
+        CommandResult result = mRunUtil.runTimedCmd(1000 * 60 * 5, cmd.split("\\s+"));
+        Assert.assertNotNull(result);
+        Assert.assertTrue(
+                String.format("Command %s failed", cmd),
+                result.getStatus().equals(CommandStatus.SUCCESS));
+        CLog.v("output:\n%s", result.getStdout());
+        return result.getStdout();
     }
 
     /**
