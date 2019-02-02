4d0f8328a8c7491685ef09da67fddb17481a1edf
Jiyong Park <jiyong@google.com>
2019-02-02 19:45:57 +0900

    apexd is started much earlier to create loopback devices
    This change fixes the problem that apexd is delaying the entire boot
    sequence while waiting for the loopback devices to be created. The delay
    was as big as 50 ms per a loopback device.
    
    With this change, apexd is started much earlier: from "on post-fs-data"
    to "on init". When it is first started, it scans /system/apex to
    determine the number of APEXes and creates that number of loopback
    devices priori. Since then it enters into the binder loop.
    
    When the data partition is mounted, init lets apexd to initiate the
    apexd boot sequence where APEXes in /data is scanned, verified, and
    activated. Since the creation of the loopback devices were requested far
    before, it is very likely that dev nodes for the devices are ready at
    this moment (even if not, this isn't a lose).
    
    Bug: 123404717
    Bug: 123772265
    Test: compare boot times.
    init_zygote_START_TIME_avg is improved from 2831ms to 2622ms on blueline
    Change-Id: Id88315c8519e4c1ce22675cdd7de89c1daa2dab9

--- apexd/apexd_main.cpp
+++ apexd/apexd_main.cpp
@@ -22,10 +22,14 @@
 
 #include "apexd.h"
 #include "apexd_prepostinstall.h"
-#include "apexd_prop.h"
 #include "apexservice.h"
 
+#include <android-base/properties.h>
+
 namespace {
+
+static constexpr const char* kApexDataStatusSysprop = "apexd.data.status";
+static constexpr const char* kApexDataStatusReady = "ready";
 
 int HandleSubcommand(char** argv) {
   if (strcmp("--pre-install", argv[1]) == 0) {
@@ -70,29 +74,12 @@ int main(int /*argc*/, char** argv) {
   // TODO: add a -v flag or an external setting to change LogSeverity.
   android::base::SetMinimumLogSeverity(android::base::VERBOSE);
 
+  // Wait for /data/apex. The property is set by init.
+  android::base::WaitForProperty(kApexDataStatusSysprop, kApexDataStatusReady);
+  android::apex::startBootSequence();
+
   android::apex::binder::CreateAndRegisterService();
-
-  android::apex::unmountAndDetachExistingImages();
-
-  android::apex::scanStagedSessionsDirAndStage();
-
-  // Scan the directory under /data first, as it may contain updates of APEX
-  // packages living in the directory under /system, and we want the former ones
-  // to be used over the latter ones.
-  android::apex::scanPackagesDirAndActivate(
-      android::apex::kActiveApexPackagesDataDir);
-  android::apex::scanPackagesDirAndActivate(
-      android::apex::kApexPackageSystemDir);
-
-  // Notify other components (e.g. init) that all APEXs are correctly mounted
-  // and are ready to be used.
-  android::apex::onAllPackagesReady();
-
   android::apex::binder::StartThreadPool();
-
-  android::apex::waitForBootStatus(android::apex::rollbackLastSession);
-
   android::apex::binder::JoinThreadPool();
-
   return 1;
 }
