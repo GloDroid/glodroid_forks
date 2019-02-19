eaf60a7c5f14f696b8256808fbaa2bfbffe1252b
Andreas Gampe <agampe@google.com>
2019-02-19 11:41:08 -0800

    Apexd: Add env test to pre- & post-install
    Check whether BOOTCLASSPATH is defined as an example for a correct
    environment.
    
    Bug: 124769206
    Test: atest apexservice_test
    Change-Id: Id2e2923c2d1e6abf91910f901620a9f8113d33bc

--- apexd/apexd_testdata/preInstallHook.sh
+++ apexd/apexd_testdata/preInstallHook.sh
@@ -1,4 +1,11 @@
 #!/system/bin/sh
+
+# Side test for b/124769206.
+if [ -z "$BOOTCLASSPATH" ] ; then
+  log -t apex_tests -p f "BOOTCLASSPATH is perceived as empty"
+  exit 1
+fi
+
 # Look for the session data, but do not fail and instead print a literal
 # otherwise. Sleep is an attempt to ensure that the message always reaches
 # logcat.
