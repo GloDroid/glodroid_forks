aec451015d49fb1d4caf588906e5117e6c4360e6
Chong Zhang <chz@google.com>
2019-02-21 15:40:04 -0800

    check that the swcodecs are installed using lshal
    Test: atest media_swcodec_e2e_tests
    Bug: 123704841
    
    Change-Id: Id6090e3485b80f31039f7ead92ed10def963e0be

--- tests/src/com/android/tests/apex/MediaSwCodecHostTest.java
+++ tests/src/com/android/tests/apex/MediaSwCodecHostTest.java
@@ -16,21 +16,34 @@
 
 package com.android.tests.apex;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 import com.android.tradefed.device.DeviceNotAvailableException;
+import com.android.tradefed.device.IManagedTestDevice;
 import com.android.tradefed.device.ITestDevice.ApexInfo;
 import com.android.tradefed.log.LogUtil.CLog;
 import com.android.tradefed.testtype.DeviceJUnit4ClassRunner;
+import com.android.tradefed.util.CommandResult;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import java.io.IOException;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Test to check if Apex can be staged, activated and uninstalled successfully.
  */
 @RunWith(DeviceJUnit4ClassRunner.class)
 public class MediaSwCodecHostTest extends ApexE2EBaseHostTest {
+    private static final String CMD_LSHAL_CODECS =
+            "lshal debug android.hardware.media.c2@1.0::IComponentStore/software";
+    private static final Pattern CODEC_NAME_REGEX =
+            Pattern.compile("\\s+name: (.+)");
+
     /**
      * Tests that if Apex package can be staged, activated and uninstalled successfully.
      */
@@ -49,7 +62,30 @@ public class MediaSwCodecHostTest extends ApexE2EBaseHostTest {
 
     @Override
     public void additionalCheck() {
-        // TODO: Add the additional check for media module.
+        assertTrue(((IManagedTestDevice) getDevice()).getMonitor().waitForBootComplete(60000));
+        checkCodecs();
     }
 
+    private void checkCodecs() {
+        try {
+            CommandResult commandResult = getDevice().executeShellV2Command(CMD_LSHAL_CODECS);
+            assertEquals("Couldn't list software codecs on device", 
+                    0, (int) commandResult.getExitCode());
+
+            String outputString = commandResult.getStdout();
+            Scanner in = new Scanner(outputString);
+            int codecCount = 0;
+            while (in.hasNextLine()) {
+                String line = in.nextLine();
+                Matcher m = CODEC_NAME_REGEX.matcher(line);
+                if (m.matches()) {
+                    codecCount++;
+                }
+            }
+            CLog.i("Found " + codecCount + " codecs");
+            assertTrue("Failed to load software codecs. ", codecCount > 0);
+        } catch (DeviceNotAvailableException e) {
+            throw new AssertionError("Unable to run lshal", e);
+        }
+    }
 }
