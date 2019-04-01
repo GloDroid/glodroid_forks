5e8102307e5664c632bfe36fd74168ab72b53ed9
Jiyong Park <jiyong@google.com>
2019-04-01 15:24:26 +0900

    APEX keys are embedded in the APEX
    APEX keys are embedded in the APEX when the APEX built. When the apexd
    starts, it scans built-in APEX directories, e.g. /system/apex, to
    collect public keys embedded in the built-in APEXes. The scanned keys
    are then used to verify APEXes during the activation.
    
    Bug: 128344735
    Test: device boots to the UI. presubmit test passes.
    Change-Id: I3bbc7d19a972ec6c79776aaa82ea8ed2346e4600

--- apexd/apexd.h
+++ apexd/apexd.h
@@ -22,6 +22,7 @@
 
 #include <android-base/macros.h>
 
+#include "apex_constants.h"
 #include "apex_file.h"
 #include "status.h"
 #include "status_or.h"
@@ -30,14 +31,6 @@ namespace android {
 namespace apex {
 
 class CheckpointInterface;
-
-static constexpr const char* kApexDataDir = "/data/apex";
-static constexpr const char* kActiveApexPackagesDataDir = "/data/apex/active";
-static constexpr const char* kApexBackupDir = "/data/apex/backup";
-static constexpr const char* kApexPackageSystemDir = "/system/apex";
-static constexpr const char* kApexPackageProductDir = "/product/apex";
-static constexpr const char* kApexRoot = "/apex";
-static constexpr const char* kStagedSessionsDir = "/data/app-staging";
 
 Status resumeRollbackIfNeeded();
 void unmountAndDetachExistingImages();
