2247d1d56781e832f79afe82637e725226a272d3
Jaewoong Jung <jungjw@google.com>
2019-06-18 13:10:28 -0700

    Add an optional flag for an assets/ dir.
    This allows supplemental resource files (e.g. NOTICE output) to be
    included in the APEX under assets.
    
    Bug: 135218846
    Test: Built Mainline modules + manual inspection
    Change-Id: Iafdbc9012ea72a2298b84e70779282c00f191f31

--- apexer/apexer.py
+++ apexer/apexer.py
@@ -51,6 +51,10 @@ def ParseArgs(argv):
       help='path to the AndroidManifest file. If omitted, a default one is created and used'
   )
   parser.add_argument(
+      '--assets_dir',
+      help='an assets directory to be included in the APEX'
+  )
+  parser.add_argument(
       '--file_contexts',
       help='selinux file contexts file. Required for "image" APEXs.')
   parser.add_argument(
@@ -397,6 +401,8 @@ def CreateApex(args, work_dir):
     cmd.extend(['--version-name', manifest_apex.versionName])
   if args.target_sdk_version:
     cmd.extend(['--target-sdk-version', args.target_sdk_version])
+  if args.assets_dir:
+    cmd.extend(['-A', args.assets_dir])
   # Default value for minSdkVersion.
   cmd.extend(['--min-sdk-version', '29'])
   cmd.extend(['-o', apk_file])
