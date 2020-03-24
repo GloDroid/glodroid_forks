decaa146e8bc52e500c7db22cd48db05dc274a2c
Icenowy Zheng <icenowy@aosc.io>
2020-03-24 19:21:58 +0800

    Android.mk: make test programs optional
    The eng target build type is obsolete and not allowed on Q.
    
    Signed-off-by: Icenowy Zheng <icenowy@aosc.io>

--- Android.mk
+++ Android.mk
@@ -57,7 +57,7 @@ LOCAL_MODULE := sens
 LOCAL_CFLAGS := -DLOG_TAG=\"Sensors\" -fvisibility=hidden
 LOCAL_SHARED_LIBRARIES := liblog libcutils libdl
 LOCAL_SRC_FILES := sens.c
-LOCAL_MODULE_TAGS := eng
+LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR_EXECUTABLES)
 include $(BUILD_EXECUTABLE)
 
@@ -88,6 +88,6 @@ LOCAL_MODULE := activity
 LOCAL_CFLAGS := -DLOG_TAG=\"Activity\" -fvisibility=hidden
 LOCAL_SHARED_LIBRARIES := liblog libcutils libdl
 LOCAL_SRC_FILES := activity.c
-LOCAL_MODULE_TAGS := eng
+LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR_EXECUTABLES)
 include $(BUILD_EXECUTABLE)
