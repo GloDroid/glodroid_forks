21a81259c0780fc92d3c626900a9f0c07b6aea3b
Kelly Rossmoyer <krossmo@google.com>
2020-04-07 12:25:33 -0700

    ANDROID: power: wakeup_reason: wake reason enhancements
    These changes build upon the existing Android kernel wakeup reason code
    to:
    * improve the positioning of suspend abort logging calls in suspend flow
    * add logging of abnormal wakeup reasons like unexpected HW IRQs and
      IRQs configured as both wake-enabled and no-suspend
    * add support for capturing deferred-processing threaded nested IRQs as
      wakeup reasons rather than their synchronously-processed parents
    
    Bug: 150970830
    Bug: 140217217
    Bug: 120445600
    
    Signed-off-by: Kelly Rossmoyer <krossmo@google.com>
    Change-Id: I903b811a0fe11a605a25815c3a341668a23de700
    Signed-off-by: Matthias Maennich <maennich@google.com>

--- drivers/base/power/main.c
+++ drivers/base/power/main.c
@@ -1235,6 +1235,8 @@ Run:
 	error = dpm_run_callback(callback, dev, state, info);
 	if (error) {
 		async_error = error;
+		log_suspend_abort_reason("Callback failed on %s in %pS returned %d",
+					 dev_name(dev), callback, error);
 		goto Complete;
 	}
 
@@ -1427,6 +1429,8 @@ Run:
 	error = dpm_run_callback(callback, dev, state, info);
 	if (error) {
 		async_error = error;
+		log_suspend_abort_reason("Callback failed on %s in %pS returned %d",
+					 dev_name(dev), callback, error);
 		goto Complete;
 	}
 	dpm_propagate_wakeup_to_parent(dev);
@@ -1594,7 +1598,6 @@ static int __device_suspend(struct device *dev, pm_message_t state, bool async)
 	pm_callback_t callback = NULL;
 	const char *info = NULL;
 	int error = 0;
-	char suspend_abort[MAX_SUSPEND_ABORT_LEN];
 	DECLARE_DPM_WATCHDOG_ON_STACK(wd);
 
 	TRACE_DEVICE(dev);
@@ -1618,9 +1621,6 @@ static int __device_suspend(struct device *dev, pm_message_t state, bool async)
 
 	if (pm_wakeup_pending()) {
 		dev->power.direct_complete = false;
-		pm_get_active_wakeup_sources(suspend_abort,
-			MAX_SUSPEND_ABORT_LEN);
-		log_suspend_abort_reason(suspend_abort);
 		async_error = -EBUSY;
 		goto Complete;
 	}
@@ -1697,6 +1697,9 @@ static int __device_suspend(struct device *dev, pm_message_t state, bool async)
 
 		dpm_propagate_wakeup_to_parent(dev);
 		dpm_clear_superiors_direct_complete(dev);
+	} else {
+		log_suspend_abort_reason("Callback failed on %s in %pS returned %d",
+					 dev_name(dev), callback, error);
 	}
 
 	device_unlock(dev);
@@ -1901,6 +1904,8 @@ int dpm_prepare(pm_message_t state)
 			}
 			pr_info("Device %s not prepared for power transition: code %d\n",
 				dev_name(dev), error);
+			log_suspend_abort_reason("Device %s not prepared for power transition: code %d",
+						 dev_name(dev), error);
 			dpm_save_failed_dev(dev_name(dev));
 			put_device(dev);
 			break;
