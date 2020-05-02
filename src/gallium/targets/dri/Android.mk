e2b75435ce4ab84eb6de227629cdc27970fca5a2
Roman Stratiienko <roman.stratiienko@nure.ua>
2020-05-02 10:29:10 +0300

    vc4: Fix Android build
    Also fixes build with both v3d and vc4 selected.
    
    Signed-off-by: Roman Stratiienko <roman.stratiienko@nure.ua>

--- src/gallium/targets/dri/Android.mk
+++ src/gallium/targets/dri/Android.mk
@@ -66,6 +66,7 @@ LOCAL_STATIC_LIBRARIES += \
 	libpanfrost_midgard \
 	libpanfrost_shared \
 	libpanfrost_util \
+	libmesa_broadcom_common \
 
 ifeq ($(USE_LIBBACKTRACE),true)
 	LOCAL_SHARED_LIBRARIES += libbacktrace
