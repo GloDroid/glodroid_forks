b01abc5f1176e43c9d18882066a7fbe7bb342648
Yun Hsiang <yun.hsiang@mediatek.com>
2020-05-20 10:31:35 +0800

    ANDROID: sched: add restrict vendor hook to modify task placement policy in EAS
    For modifying task placement policy, we add the hook on the top of
    select_task_rq_fair(). It allows us to modify wakeup/fork/exec balance
    paths.
    
    Bug: 158263250
    Signed-off-by: Yun Hsiang <yun.hsiang@mediatek.com>
    Change-Id: I80ff870453472e183ab2aae7381bff91e49ae296
    Signed-off-by: Matthias Maennich <maennich@google.com>

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -8,6 +8,7 @@
 
 #define CREATE_TRACE_POINTS
 #include <trace/hooks/vendor_hooks.h>
+#include <trace/hooks/sched.h>
 #include <trace/hooks/fpsimd.h>
 #include <trace/hooks/rwsem.h>
 #include <trace/hooks/futex.h>
@@ -17,6 +18,7 @@
  * Export tracepoints that act as a bare tracehook (ie: have no trace event
  * associated with them) to allow external modules to probe them.
  */
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_select_task_rq_fair);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_is_fpsimd_save);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_rwsem_init);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_rwsem_wake);
