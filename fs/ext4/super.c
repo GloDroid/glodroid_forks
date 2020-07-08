de01207111be6cb819b7dd2af592e50d88736ce2
Daniel Rosenberg <drosen@google.com>
2020-07-08 02:12:37 -0700

    FROMLIST: ext4: Use generic casefolding support
    This switches ext4 over to the generic support provided in
    the previous patch.
    
    Since casefolded dentries behave the same in ext4 and f2fs, we decrease
    the maintenance burden by unifying them, and any optimizations will
    immediately apply to both.
    
    Signed-off-by: Daniel Rosenberg <drosen@google.com>
    Reviewed-by: Eric Biggers <ebiggers@google.com>
    
    Link: https://lore.kernel.org/r/20200708091237.3922153-5-drosen@google.com
    Bug: 138322712
    Bug: 161184936
    Test: Boots, /data/media is case insensitive
    Change-Id: I3a0705278100590df4c7cdd0dcdf945e9f11feb7
    Signed-off-by: Matthias Maennich <maennich@google.com>

--- fs/ext4/super.c
+++ fs/ext4/super.c
@@ -1102,7 +1102,7 @@ static void ext4_put_super(struct super_block *sb)
 	fs_put_dax(sbi->s_daxdev);
 	fscrypt_free_dummy_context(&sbi->s_dummy_enc_ctx);
 #ifdef CONFIG_UNICODE
-	utf8_unload(sbi->s_encoding);
+	utf8_unload(sb->s_encoding);
 #endif
 	kfree(sbi);
 }
@@ -4035,7 +4035,7 @@ static int ext4_fill_super(struct super_block *sb, void *data, int silent)
 		goto failed_mount;
 
 #ifdef CONFIG_UNICODE
-	if (ext4_has_feature_casefold(sb) && !sbi->s_encoding) {
+	if (ext4_has_feature_casefold(sb) && !sb->s_encoding) {
 		const struct ext4_sb_encodings *encoding_info;
 		struct unicode_map *encoding;
 		__u16 encoding_flags;
@@ -4066,8 +4066,8 @@ static int ext4_fill_super(struct super_block *sb, void *data, int silent)
 			 "%s-%s with flags 0x%hx", encoding_info->name,
 			 encoding_info->version?:"\b", encoding_flags);
 
-		sbi->s_encoding = encoding;
-		sbi->s_encoding_flags = encoding_flags;
+		sb->s_encoding = encoding;
+		sb->s_encoding_flags = encoding_flags;
 	}
 #endif
 
@@ -4678,7 +4678,7 @@ no_journal:
 	}
 
 #ifdef CONFIG_UNICODE
-	if (sbi->s_encoding)
+	if (sb->s_encoding)
 		sb->s_d_op = &ext4_dentry_ops;
 #endif
 
@@ -4873,7 +4873,7 @@ failed_mount:
 		crypto_free_shash(sbi->s_chksum_driver);
 
 #ifdef CONFIG_UNICODE
-	utf8_unload(sbi->s_encoding);
+	utf8_unload(sb->s_encoding);
 #endif
 
 #ifdef CONFIG_QUOTA
