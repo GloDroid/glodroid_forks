89bd508d1f11a4a9aca9d92eb47d1ba3d2d2a029
Eric Biggers <ebiggers@google.com>
2020-07-13 18:19:11 -0700

    ANDROID: fs: adjust casefolding support to match android-mainline
    Make some changes on top of the latest FROMLIST generic casefolding
    support patches to match what's currently in android-mainline.
    
    This commit should be reverted eventually in order to update to the
    latest version of the patches.
    
    Bug: 138322712
    Bug: 161184936
    Change-Id: I16ae27775f6df7faa53efb50917a7063a415d0f1
    Signed-off-by: Eric Biggers <ebiggers@google.com>
    Signed-off-by: Matthias Maennich <maennich@google.com>

--- fs/libfs.c
+++ fs/libfs.c
@@ -1367,38 +1367,27 @@ bool is_empty_dir_inode(struct inode *inode)
 }
 
 #ifdef CONFIG_UNICODE
-/*
- * Determine if the name of a dentry should be casefolded.
- *
- * Return: if names will need casefolding
- */
-static bool needs_casefold(const struct inode *dir)
+bool needs_casefold(const struct inode *dir)
 {
-	return IS_CASEFOLDED(dir) && dir->i_sb->s_encoding;
+	return IS_CASEFOLDED(dir) && dir->i_sb->s_encoding &&
+			(!IS_ENCRYPTED(dir) || fscrypt_has_encryption_key(dir));
 }
+EXPORT_SYMBOL(needs_casefold);
 
-/**
- * generic_ci_d_compare - generic d_compare implementation for casefolding filesystems
- * @dentry:	dentry whose name we are checking against
- * @len:	len of name of dentry
- * @str:	str pointer to name of dentry
- * @name:	Name to compare against
- *
- * Return: 0 if names match, 1 if mismatch, or -ERRNO
- */
 int generic_ci_d_compare(const struct dentry *dentry, unsigned int len,
 			  const char *str, const struct qstr *name)
 {
 	const struct dentry *parent = READ_ONCE(dentry->d_parent);
-	const struct inode *dir = READ_ONCE(parent->d_inode);
+	const struct inode *inode = READ_ONCE(parent->d_inode);
 	const struct super_block *sb = dentry->d_sb;
 	const struct unicode_map *um = sb->s_encoding;
-	struct qstr qstr = QSTR_INIT(str, len);
+	struct qstr entry = QSTR_INIT(str, len);
 	char strbuf[DNAME_INLINE_LEN];
 	int ret;
 
-	if (!dir || !needs_casefold(dir))
+	if (!inode || !needs_casefold(inode))
 		goto fallback;
+
 	/*
 	 * If the dentry name is stored in-line, then it may be concurrently
 	 * modified by a rename.  If this happens, the VFS will eventually retry
@@ -1409,15 +1398,16 @@ int generic_ci_d_compare(const struct dentry *dentry, unsigned int len,
 	if (len <= DNAME_INLINE_LEN - 1) {
 		memcpy(strbuf, str, len);
 		strbuf[len] = 0;
-		qstr.name = strbuf;
+		entry.name = strbuf;
 		/* prevent compiler from optimizing out the temporary buffer */
 		barrier();
 	}
-	ret = utf8_strncasecmp(um, name, &qstr);
+
+	ret = utf8_strncasecmp(um, name, &entry);
 	if (ret >= 0)
 		return ret;
 
-	if (sb_has_strict_encoding(sb))
+	if (sb_has_enc_strict_mode(sb))
 		return -EINVAL;
 fallback:
 	if (len != name->len)
@@ -1426,27 +1416,27 @@ fallback:
 }
 EXPORT_SYMBOL(generic_ci_d_compare);
 
-/**
- * generic_ci_d_hash - generic d_hash implementation for casefolding filesystems
- * @dentry:	dentry of the parent directory
- * @str:	qstr of name whose hash we should fill in
- *
- * Return: 0 if hash was successful or unchanged, and -EINVAL on error
- */
 int generic_ci_d_hash(const struct dentry *dentry, struct qstr *str)
 {
-	const struct inode *dir = READ_ONCE(dentry->d_inode);
+	const struct inode *inode = READ_ONCE(dentry->d_inode);
 	struct super_block *sb = dentry->d_sb;
 	const struct unicode_map *um = sb->s_encoding;
 	int ret = 0;
 
-	if (!dir || !needs_casefold(dir))
+	if (!inode || !needs_casefold(inode))
 		return 0;
 
 	ret = utf8_casefold_hash(um, dentry, str);
-	if (ret < 0 && sb_has_strict_encoding(sb))
-		return -EINVAL;
+	if (ret < 0)
+		goto err;
+
 	return 0;
+err:
+	if (sb_has_enc_strict_mode(sb))
+		ret = -EINVAL;
+	else
+		ret = 0;
+	return ret;
 }
 EXPORT_SYMBOL(generic_ci_d_hash);
 
