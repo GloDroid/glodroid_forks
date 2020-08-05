33fa10cbe73d4a38a64c317b1ef374eb28d3a9f8
Sangmoon Kim <sangmoon.kim@samsung.com>
2020-08-05 22:13:43 +0900

    ANDROID: vendor_hooks: add waiting information for blocked tasks
     - Add the hook to get mutex/rwsem information that the tasks
       are waiting for.
    
     - Add the hook to print messages for sched_show_task.
    
     - ANDROID_VENDOR_DATA_ARRAY added to task_struct
    
    Bug: 162776704
    
    Signed-off-by: Sangmoon Kim <sangmoon.kim@samsung.com>
    Change-Id: Ib436fbd8d0ad509c3b5a73ea8f5170e0761a13fd
    (cherry picked from commit b519ac423787d38f467ca479d2126b7204d6f498)
    Signed-off-by: Matthias Maennich <maennich@google.com>

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -12,6 +12,7 @@
 #include <trace/hooks/fpsimd.h>
 #include <trace/hooks/rwsem.h>
 #include <trace/hooks/futex.h>
+#include <trace/hooks/dtask.h>
 #include <trace/hooks/mpam.h>
 
 /*
@@ -37,4 +38,11 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_rwsem_wake);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_rwsem_write_finished);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_alter_rwsem_list_add);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_alter_futex_plist_add);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_mutex_wait_start);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_mutex_wait_finish);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_rwsem_read_wait_start);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_rwsem_read_wait_finish);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_rwsem_write_wait_start);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_rwsem_write_wait_finish);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_sched_show_task);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_mpam_set);
