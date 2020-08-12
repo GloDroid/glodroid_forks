7c80730c93e2744c48989d7adae71c2c0d9a6502
Todd Kjos <tkjos@google.com>
2020-08-12 08:31:55 -0700

    Revert "sched: Remove sched_setscheduler*() EXPORTs"
    This reverts commit 616d91b68cd56bcb1954b6a5af7d542401fde772.
    
    Some vendor modules need to set explicit priorities for kernel
    threads (display, audio, graphics)
    
    Bug: 163613349
    Signed-off-by: Todd Kjos <tkjos@google.com>
    Change-Id: I58cadfd975af8b2911c05b61e6a3fa184efb1a8e
    Signed-off-by: Matthias Maennich <maennich@google.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -5498,11 +5498,13 @@ int sched_setscheduler(struct task_struct *p, int policy,
 {
 	return _sched_setscheduler(p, policy, param, true);
 }
+EXPORT_SYMBOL_GPL(sched_setscheduler);
 
 int sched_setattr(struct task_struct *p, const struct sched_attr *attr)
 {
 	return __sched_setscheduler(p, attr, true, true);
 }
+EXPORT_SYMBOL_GPL(sched_setattr);
 
 int sched_setattr_nocheck(struct task_struct *p, const struct sched_attr *attr)
 {
@@ -5527,6 +5529,7 @@ int sched_setscheduler_nocheck(struct task_struct *p, int policy,
 {
 	return _sched_setscheduler(p, policy, param, false);
 }
+EXPORT_SYMBOL_GPL(sched_setscheduler_nocheck);
 
 /*
  * SCHED_FIFO is a broken scheduler model; that is, it is fundamentally
