4de984a1a144a0d48e1d3e894f4c4906a7a53693
Roman Stratiienko <r.stratiienko@gmail.com>
2021-06-23 17:14:23 +0300

    minigbm: Add dmabuf_heap-based backend
    WIP at this moment.
    Working on RPI4. (v3d+vc4).
    
    Signed-off-by: Roman Stratiienko <r.stratiienko@gmail.com>
    Change-Id: I517f095543b031e35f0d48bc03f6010a67012865

---
+++ dmabuf_driver/dmabuf_external_driver.cpp
@@ -0,0 +1,31 @@
+#include "dmabuf_internals.h"
+
+#include "drv_priv.h"
+#include "util.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+struct backend dmabuf_backend = {
+	.name = "dmabuf",
+	.init = dmabuf_driver_init,
+	.close = dmabuf_driver_close,
+	.bo_create = dmabuf_bo_create,
+	.bo_destroy = dmabuf_bo_destroy,
+	.bo_import = dmabuf_bo_import,
+	.bo_map = dmabuf_bo_map,
+	.bo_unmap = dmabuf_bo_unmap,
+	.bo_flush = dmabuf_bo_flush,
+	.resolve_format_and_use_flags = dmabuf_resolve_format_and_use_flags,
+	.bo_get_plane_fd = dmabuf_bo_get_plane_fd,
+};
+
+struct backend *init_external_backend(int *fd)
+{
+	return &dmabuf_backend;
+}
+
+#ifdef __cplusplus
+}
+#endif
