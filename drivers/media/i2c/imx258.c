d16bf10c0bbe17c4625d0cbc0e8cf4a80dea609c
Ondrej Jirman <megous@megous.com>
2021-10-22 18:11:26 +0200

    media: i2c: imx258: Don't be too strict about clock rate
    On Pinephone Pro, we are not able to set 19.2MHz precisely.
    Allow some slack in clock rate.
    
    Signed-off-by: Ondrej Jirman <megous@megous.com>

--- drivers/media/i2c/imx258.c
+++ drivers/media/i2c/imx258.c
@@ -79,7 +79,9 @@
 #define REG_CONFIG_FLIP_TEST_PATTERN	0x02
 
 /* Input clock frequency in Hz */
+#define IMX258_INPUT_CLOCK_FREQ_MIN	19000000
 #define IMX258_INPUT_CLOCK_FREQ		19200000
+#define IMX258_INPUT_CLOCK_FREQ_MAX	19400000
 
 struct imx258_reg {
 	u16 address;
@@ -1258,6 +1260,7 @@ static void imx258_free_controls(struct imx258 *imx258)
 static int imx258_probe(struct i2c_client *client)
 {
 	struct imx258 *imx258;
+	unsigned long clk_rate;
 	int ret;
 	u32 val = 0;
 
@@ -1277,8 +1280,12 @@ static int imx258_probe(struct i2c_client *client)
 		return dev_err_probe(&client->dev, PTR_ERR(imx258->clk),
 				     "error getting clock\n");
 	}
-	if (clk_get_rate(imx258->clk) != IMX258_INPUT_CLOCK_FREQ) {
-		dev_err(&client->dev, "input clock frequency not supported\n");
+
+	clk_rate = clk_get_rate(imx258->clk);
+	if (clk_rate < IMX258_INPUT_CLOCK_FREQ_MIN
+		|| clk_rate > IMX258_INPUT_CLOCK_FREQ_MAX) {
+		dev_err(&client->dev, "input clock frequency %lu not supported\n",
+			clk_rate);
 		return -EINVAL;
 	}
 
