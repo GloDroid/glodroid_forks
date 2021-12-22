cef5c77ef971604d2c513c1056222fbb695da1b9
Roman Stratiienko <roman.o.stratiienko@globallogic.com>
2021-12-22 16:22:24 +0200

    drm_hwcomposer: CI: Replace shell scripts with Makefile
    This won't make much benefits for CI, but for local native builds will
    significantly speed-up build cycle by allowing parallel build and by
    using dependency tracking logic.
    
    Signed-off-by: Roman Stratiienko <roman.o.stratiienko@globallogic.com>

--- presubmit.sh
+++ presubmit.sh
@@ -4,18 +4,10 @@ set -e
 
 echo "Run native build:"
 
-./.ci/.gitlab-ci-clang-build.sh
+make -f .ci/Makefile -j12
 
 echo "Run style check:"
 
 ./.ci/.gitlab-ci-checkcommit.sh
-
-echo "Run coarse clang-tidy check:"
-
-./.ci/.gitlab-ci-clang-tidy-coarse.sh
-
-echo "Run fine clang-tidy check:"
-
-./.ci/.gitlab-ci-clang-tidy-fine.sh
 
 echo -e "\n\e[32m --- SUCCESS ---"
