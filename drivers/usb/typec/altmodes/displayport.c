c9f3a6e86d08db0eec9ec2a78b6ba1401d739cd4
Ondrej Jirman <megi@xff.cz>
2022-03-23 14:04:53 +0100

    usb: typec: altmode: Fix display port alt mode
    ... for squashing up
    
    Signed-off-by: Ondrej Jirman <megi@xff.cz>

--- drivers/usb/typec/altmodes/displayport.c
+++ drivers/usb/typec/altmodes/displayport.c
@@ -93,26 +93,32 @@ static int dp_altmode_configure(struct dp_altmode *dp, u8 con)
 		return 0;
 	case DP_STATUS_CON_DFP_D:
 		conf |= DP_CONF_UFP_U_AS_DFP_D;
-		pin_assign = dp->port->vdo & DP_CAP_RECEPTACLE ?
-			DP_CAP_DFP_D_PIN_ASSIGN(dp->port->vdo) :
-			DP_CAP_UFP_D_PIN_ASSIGN(dp->port->vdo);
-		pin_assign &= dp->alt->vdo & DP_CAP_RECEPTACLE ?
-			DP_CAP_UFP_D_PIN_ASSIGN(dp->alt->vdo) :
-			DP_CAP_DFP_D_PIN_ASSIGN(dp->alt->vdo);
+		pin_assign = DP_CAP_UFP_D_PIN_ASSIGN(dp->alt->vdo) &
+			     DP_CAP_DFP_D_PIN_ASSIGN(dp->port->vdo);
 		break;
 	case DP_STATUS_CON_UFP_D:
 	case DP_STATUS_CON_BOTH: /* NOTE: First acting as DP source */
 		conf |= DP_CONF_UFP_U_AS_UFP_D;
-		pin_assign = dp->port->vdo & DP_CAP_RECEPTACLE ?
-			DP_CAP_UFP_D_PIN_ASSIGN(dp->port->vdo) :
-			DP_CAP_DFP_D_PIN_ASSIGN(dp->port->vdo);
+		/*
+		 * The Display Port Alt mode standard is not publicly available,
+		 * so this is based on guesswork and real VDOs received from
+		 * receptacle based and plug based Type-C alt mode supporting
+		 * docks to make configuration work in practice:
+		 *
+		 * Plug (captive cable) based dock: port=c46 alt=c05
+		 * Recpetacle based dock: port=c46 alt=c0045
+		 */
+		pin_assign = DP_CAP_DFP_D_PIN_ASSIGN(dp->port->vdo);
 		pin_assign &= dp->alt->vdo & DP_CAP_RECEPTACLE ?
-			DP_CAP_DFP_D_PIN_ASSIGN(dp->alt->vdo) :
-			DP_CAP_UFP_D_PIN_ASSIGN(dp->alt->vdo);
+			DP_CAP_UFP_D_PIN_ASSIGN(dp->alt->vdo) :
+			DP_CAP_DFP_D_PIN_ASSIGN(dp->alt->vdo);
 		break;
 	default:
 		break;
 	}
+
+	dev_info(&dp->alt->dev, "con=%d pin_assign=%x (port=%x alt=%x)\n",
+		 (int)con, (unsigned)pin_assign, dp->port->vdo, dp->alt->vdo);
 
 	/* Determining the initial pin assignment. */
 	if (!DP_CONF_GET_PIN_ASSIGN(dp->data.conf)) {
