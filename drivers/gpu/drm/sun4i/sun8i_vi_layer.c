33e843840abb4cf322e2a1a81facc450be581344
Roman Stratiienko <r.stratiienko@gmail.com>
2022-05-28 09:41:49 +0300

    Revert "drm/sun4i: Support for premulti/coverage blending modes"
    This reverts commit e4240f63816b3bd7ab113c2e601117010704d235.

--- drivers/gpu/drm/sun4i/sun8i_vi_layer.c
+++ drivers/gpu/drm/sun4i/sun8i_vi_layer.c
@@ -104,13 +104,11 @@ static void sun8i_vi_layer_enable(struct sun8i_mixer *mixer, int channel,
 }
 
 static void sun8i_vi_layer_update_alpha(struct sun8i_mixer *mixer, int channel,
-					int overlay, struct drm_plane *plane,
-					unsigned int zpos)
+					int overlay, struct drm_plane *plane)
 {
-	u32 mask, val, ch_base, bld_base;
+	u32 mask, val, ch_base;
 
 	ch_base = sun8i_channel_base(mixer, channel);
-	bld_base = sun8i_blender_base(mixer);
 
 	if (mixer->cfg->is_de3) {
 		mask = SUN50I_MIXER_CHAN_VI_LAYER_ATTR_ALPHA_MASK |
@@ -133,13 +131,6 @@ static void sun8i_vi_layer_update_alpha(struct sun8i_mixer *mixer, int channel,
 				   SUN8I_MIXER_FCC_GLOBAL_ALPHA
 					(plane->state->alpha >> 8));
 	}
-
-	regmap_update_bits(mixer->engine.regs,
-			   SUN8I_MIXER_BLEND_PREMULTIPLY(bld_base),
-			   SUN8I_MIXER_BLEND_PREMULTIPLY_EN(zpos),
-			   (mixer->cfg->is_de3) ?
-				SUN8I_MIXER_BLEND_PREMULTIPLY_EN(zpos) : 0);
-
 }
 
 static int sun8i_vi_layer_update_coord(struct sun8i_mixer *mixer, int channel,
@@ -473,7 +464,7 @@ static void sun8i_vi_layer_atomic_update(struct drm_plane *plane,
 		sun8i_vi_layer_update_coord(mixer, layer->channel,
 					    layer->overlay, plane, zpos);
 		sun8i_vi_layer_update_alpha(mixer, layer->channel,
-					    layer->overlay, plane, zpos);
+					    layer->overlay, plane);
 		sun8i_vi_layer_update_formats(mixer, layer->channel,
 					      layer->overlay, plane);
 		sun8i_vi_layer_update_buffer(mixer, layer->channel,
@@ -646,12 +637,6 @@ struct sun8i_vi_layer *sun8i_vi_layer_init_one(struct drm_device *drm,
 			return ERR_PTR(ret);
 		}
 	}
-
-	if (mixer->cfg->is_de3)
-		drm_plane_create_blend_mode_property
-			(&layer->plane, BIT(DRM_MODE_BLEND_PREMULTI) |
-			 BIT(DRM_MODE_BLEND_COVERAGE) |
-			 BIT(DRM_MODE_BLEND_PIXEL_NONE));
 
 	ret = drm_plane_create_zpos_property(&layer->plane, index,
 					     0, plane_cnt - 1);
