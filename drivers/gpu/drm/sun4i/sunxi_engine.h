328f849d4b3276e731dd32887f55143ce013d0eb
Roman Stratiienko <r.stratiienko@gmail.com>
2022-05-28 09:40:47 +0300

    Revert "drm/sun4i: Add mode_set callback to the engine"
    This reverts commit 5f43312f15d4cac5e62acbef18dffcf16d5dcab1.

--- drivers/gpu/drm/sun4i/sunxi_engine.h
+++ drivers/gpu/drm/sun4i/sunxi_engine.h
@@ -9,7 +9,6 @@
 struct drm_plane;
 struct drm_device;
 struct drm_crtc_state;
-struct drm_display_mode;
 
 struct sunxi_engine;
 
@@ -109,17 +108,6 @@ struct sunxi_engine_ops {
 	 * This function is optional.
 	 */
 	void (*vblank_quirk)(struct sunxi_engine *engine);
-
-	/**
-	 * @mode_set:
-	 *
-	 * This callback is used to update engine registers that
-	 * responsible for display frame size and other mode attributes.
-	 *
-	 * This function is optional.
-	 */
-	void (*mode_set)(struct sunxi_engine *engine,
-			 struct drm_display_mode *mode);
 };
 
 /**
