218c991919622e15bf49c30744f0877596384b36
Ondrej Jirman <megi@xff.cz>
2022-08-27 20:48:32 +0200

    ASoC: codec: rt5640: Resolve failure to set DMIC clock after playback
    rt5640_set_dai_sysclk is called with freq == 0 when playback stops. This
    causes DMIC setup code to fail. I2S interface doesn't need to be active
    for codec to work, so don't clear rt5640->sysclk after rt5640_set_dai_sysclk
    is called with freq == 0.
    
    Signed-off-by: Ondrej Jirman <megi@xff.cz>

--- sound/soc/codecs/rt5640.c
+++ sound/soc/codecs/rt5640.c
@@ -1840,6 +1840,9 @@ static int rt5640_set_dai_sysclk(struct snd_soc_dai *dai,
 	unsigned int reg_val = 0;
 	unsigned int pll_bit = 0;
 
+	if (freq == 0)
+		return 0;
+
 	switch (clk_id) {
 	case RT5640_SCLK_S_MCLK:
 		reg_val |= RT5640_SCLK_SRC_MCLK;
