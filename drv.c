6440d6c729aee4af7be824c1c8783a972b0fea1a
Roman Stratiienko <r.stratiienko@gmail.com>
2022-12-22 21:51:35 +0200

    minigbm: make dumb drivers optional
    Change-Id: Ie668d4a9d8f18ff1f885979ebe1dcd0a7646fda2
    Signed-off-by: Roman Stratiienko <r.stratiienko@gmail.com>

--- drv.c
+++ drv.c
@@ -51,6 +51,7 @@ extern const struct backend backend_vc4;
 #endif
 
 // Dumb / generic drivers
+#ifdef DRV_DUMB
 extern const struct backend backend_evdi;
 extern const struct backend backend_marvell;
 extern const struct backend backend_mediatek;
@@ -64,6 +65,7 @@ extern const struct backend backend_synaptics;
 extern const struct backend backend_virtgpu;
 extern const struct backend backend_udl;
 extern const struct backend backend_vkms;
+#endif
 
 static const struct backend *drv_backend_list[] = {
 #ifdef DRV_AMDGPU
@@ -78,10 +80,12 @@ static const struct backend *drv_backend_list[] = {
 #ifdef DRV_VC4
 	&backend_vc4,
 #endif
+#ifdef DRV_DUMB
 	&backend_evdi,	    &backend_komeda,	&backend_marvell, &backend_mediatek,
 	&backend_meson,	    &backend_nouveau,	&backend_radeon,  &backend_rockchip,
 	&backend_sun4i_drm, &backend_synaptics, &backend_udl,	  &backend_virtgpu,
 	&backend_vkms
+#endif
 };
 
 void drv_preload(bool load)
